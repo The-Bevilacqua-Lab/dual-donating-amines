580ff2f6b3f2428e8a4bd108ecef5560ab40f364
Drew <56976921+drew161@users.noreply.github.com>
2023-11-16 11:54:04 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as f:
+    requirements = [line.strip() for line in f]
+
+setup(
+    name='dual-H-bonding-nucleobases',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/drew161/dual-H-bonding-nucleobases',
+    author='Andrew Veenis',
+    description='A computational pipeline to identify and analyze nucleobases that donate two H-bonds via their exocyclic amines.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=requirements
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: MacOS',
+    ],
+)
