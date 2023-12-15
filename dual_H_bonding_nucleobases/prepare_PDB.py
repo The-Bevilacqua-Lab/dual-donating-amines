207793458c3e0400af7eb99618e4459c926882ca
drew161 <ajv6@psu.edu>
2023-12-15 10:05:12 -0500

    continued building prepare_PDB.py, added more comments and filled out the docstring

--- dual_H_bonding_nucleobases/prepare_PDB.py
+++ dual_H_bonding_nucleobases/prepare_PDB.py
@@ -1,10 +1,14 @@
 """
-This module will eventually do something.
+This module reads information on the equivalence classes in the representative set file. For each equivalence class, it
+works with PyMOL to save a mmCIF file of the entire structure containing the representative RNA chain(s) to the
+mmCIF_files folder and a PDB file of either the entire structure or a portion of the structure containing the
+representative RNA chain(s) to the PDB_files folder. In addition to the representative RNA chain(s), the PDB file
+contains other chains that are close to the representative RNA chain(s). Also, alternate conformations are removed such
+that each atom in the resulting PDB file only has a single conformation.
 """
 
 import sys
 import os
-import csv
 from pymol import cmd
 from pymol import stored
 import parse_nrlist
@@ -31,6 +35,7 @@ if os.path.isdir(mmCIF_directory):
 else:
     os.mkdir(mmCIF_directory)
 
+# move into the mmCIF_files folder so that PyMOL drops the mmCIF files into this folder when running fetch
 os.chdir(mmCIF_directory)
 
 # check whether a PDB_files folder exists
@@ -44,6 +49,7 @@ if os.path.isdir(PDB_directory):
 else:
     os.mkdir(PDB_directory)
 
+# work with each equivalence class specified in the representative set file
 for eq_class in nrlist_info:
     rep_struct_list = []
     for i in range(len(eq_class[2])):
@@ -56,21 +62,26 @@ for eq_class in nrlist_info:
         rep_struct_list.append(f"chain {eq_class[3][i]}")
     rep_struct = " ".join(rep_struct_list)
     cmd.fetch(eq_class[1][0])
-    # create a copy of the structure of just the model (state in PyMOL) indicated in the representative set file
-    cmd.create('copy', selection=eq_class[1][0], source_state=eq_class[2][0], target_state=1)
+    # create a copy of just the model (or "state" in PyMOL) indicated in the representative set file only including the
+    # representative RNA chains and any chains within 5 angstroms of them
+    cmd.create('copy', selection=f'bychain {eq_class[1][0]} within 5 of ({rep_struct})', source_state=eq_class[2][0],
+               target_state=1)
     cmd.delete(eq_class[1][0])
+    # store atoms that have alternate conformations
     stored.alt_atoms = []
-    cmd.iterate_state(0, 'not alt ""', 'stored.alt_atoms.append([ID,q,alt,state,chain,resi,name])')
+    cmd.iterate_state(0, 'not alt ""', 'stored.alt_atoms.append([ID,q,alt,chain,resi,name])')
+    # prepare a list of atom groups
+    # each atom group consists of one or more different conformations of the same atom
     alt_atoms_grouped = []
     for atom in stored.alt_atoms:
         match = False
         for group in alt_atoms_grouped:
-            if atom[3] == group[0][3] and atom[4] == group[0][4] and atom[5] == group[0][5] and atom[6] == group[0][6]:
+            if atom[3] == group[0][3] and atom[4] == group[0][4] and atom[5] == group[0][5]:
                 group.append(atom)
                 match = True
         if not match:
             alt_atoms_grouped.append([atom])
-    # determine which atoms to keep, prioritizing highest occupancy factor and lowest alt ID, in that order
+    # determine which atom conformations to keep, prioritizing highest occupancy factor and lowest alt ID, in that order
     atoms_to_remove = []
     for group in alt_atoms_grouped:
         keep = []
@@ -99,7 +110,8 @@ for eq_class in nrlist_info:
                 remove.append(atom)
         for atom in remove:
             atoms_to_remove.append(atom[0])
+    # remove the atom conformations that are not going to be kept
     for atom_ID in atoms_to_remove:
         cmd.remove(f'ID {atom_ID}')
-    # cmd.save(f'{PDB_directory}/{eq_class[0]}.pdb', state=eq_class[2][0])
+    cmd.save(f'{PDB_directory}/{eq_class[0]}.pdb')
     # cmd.delete('all')
