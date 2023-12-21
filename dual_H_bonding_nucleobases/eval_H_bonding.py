1b5caf2d938996a12dc9f28831e47cee73db6873
drew161 <ajv6@psu.edu>
2023-12-21 10:38:54 -0500

    restarted construction of residue library in eval_H_bonding.py

--- dual_H_bonding_nucleobases/eval_H_bonding.py
+++ dual_H_bonding_nucleobases/eval_H_bonding.py
@@ -5,422 +5,112 @@ This module will eventaully do something.
 from pymol import cmd
 from pymol import stored
 
-
-# adpated from pdb_miner.py from rare-nucleobase-miner repository
-residues = {
-    "ARG": {
-        "donors": {
-            "name": ["N", "NE", "NH1", "NH2"],
-            "trail": ["CA", "CZ", "CZ", "CZ"],
-            "element": ["N", "N", "N", "N"],
-            "rotatable": [False, False, False, False],
-            "state": ["N", "N", "N", "N"]
-        },
-        "acceptors": {
-            "name": ["O"],
-            "element": ["O"],
-            "state": ["N"]
-        }
+# define a list of dictionaries that provides information on the canonical amino acid and RNA/DNA residues
+# the indices of each donor or acceptor atom list specifies the following:
+#     0: donor/acceptor atom name
+#     1: donor/acceptor element
+#     2: is the donor/acceptor rotatable?
+#     3: donor/acceptor formal charge
+#     4: antecedent atom of donor/acceptor
+residues = [
+    {
+        'res': 'ALA',
+        'don': [['N', 'N', False, 0, 'CA']],
+        'acc': [['O', 'O', False, 0, 'C']]
     },
-    "LYS": {
-        "donors": {
-            "name": ["N", "NZ"],
-            "trail": ["CA", "CE"],
-            "element": ["N", "N"],
-            "rotatable": [False, True],
-            "state": ["N", "N"]
-        },
-        "acceptors": {
-            "name": ["O"],
-            "element": ["O"],
-            "state": ["N"]
-        }
+    {
+        'res': 'ASP',
+        'don': [['N', 'N', False, 0, 'CA']],
+        'acc': [['O', 'O', False, 0, 'C'], ['OD1', 'O', False, 0, 'CG'], ['OD2', 'O', False, -1, 'CG']]
     },
-    "HIS": {
-        "donors": {
-            "name": ["N", "ND1", "NE2"],
-            "trail": ["CA", "CG", "CD2"],
-            "element": ["N", "N", "N"],
-            "rotatable": [False, False, False],
-            "state": ["N", "N", "N"]
-        },
-        "acceptors": {
-            "name": ["O", "ND1", "NE2"],
-            "element": ["O", "N", "N"],
-            "state": ["N", "N", "N"]
-        }
+    {
+        'res': 'ASN',
+        'don': [['N', 'N', False, 0, 'CA'], ['ND2', 'N', False, 0, 'CG']],
+        'acc': [['O', 'O', False, 0, 'C'], ['OD1', 'O', False, 0, 'CG']]
     },
-    "TRP": {
-        "donors": {
-            "name": ["N", "NE1"],
-            "trail": ["CA", "CD1"],
-            "element": ["N", "N"],
-            "rotatable": [False, False],
-            "state": ["N", "N"]
-        },
-        "acceptors": {
-            "name": ["O"],
-            "element": ["O"],
-            "state": ["N"]
-        }
+    {
+        'res': 'ARG',
+        'don': [['N', 'N', False, 0, 'CA'], ['NE', 'N', False, 0, 'CD'], ['NH1', 'N', False, 1, 'CZ'], ['NH2', 'N', False, 0, 'CZ']],
+        'acc': [['O', 'O', False, 0, 'C']]
     },
-    "ASP": {
-        "donors": {
-            "name": ["N"],
-            "trail": ["CA"],
-            "element": ["N"],
-            "rotatable": [False],
-            "state": ["N"]
-        },
-        "acceptors": {
-            "name": ["O", "OD1", "OD2"],
-            "element": ["O", "O", "O"],
-            "state": ["N", "N", "N"]
-        }
+    {
+        'res': 'CYS',
+        'don': [['N', 'N', False, 0, 'CA'], ['SG', 'S', True, 0, 'CB']],
+        'acc': [['O', 'O', False, 0, 'C'], ['SG', 'S', True, 0, 'CB']]
     },
-    "GLU": {
-        "donors": {
-            "name": ["N"],
-            "trail": [],
-            "trail": [["CA", "CB"]],
-            "element": ["N"],
-            "rotatable": [False],
-            "state": ["N"]
-        },
-        "acceptors": {
-            "name": ["O", "OE1", "OE2"],
-            "element": ["O", "O", "O"],
-            "state": ["N", "N", "N"]
-        }
+    {
+        'res': 'GLU',
+        'don': [['N', 'N', False, 0, 'CA']],
+        'acc': [['O', 'O', False, 0, 'C'], ['OE1', 'O', False, 0, 'CD'], ['OE2', 'O', False, -1, 'CD']]
     },
-    "SER": {
-        "donors": {
-            "name": ["N", "OG"],
-            "trail": [],
-            "trail": [["CA", "CB"], ["CB", "CA"]],
-            "element": ["N", "O"],
-            "rotatable": [False, True],
-            "state": ["N", "N"]
-        },
-        "acceptors": {
-            "name": ["O", "OG"],
-            "element": ["O", "O"],
-            "state": ["N", "N"]
-        },
+    {
+        'res': 'GLN',
+        'don': [['N', 'N', False, 0, 'CA'], ['NE2', 'N', False, 0, 'CD']],
+        'acc': [['O', 'O', False, 0, 'C'], ['OE1', 'O', False, 0, 'CD']]
     },
-    "CYS": {
-        "donors": {
-            "name": ["N", "SG"],
-            "trail": [],
-            "trail": [["CA", "CB"], ["CB", "CA"]],
-            "element": ["N", "S"],
-            "rotatable": [False, True],
-            "state": ["N", "N"]
-        },
-        "acceptors": {
-            "name": ["O", "SG"],
-            "element": ["O", "S"],
-            "state": ["N", "N"]
-        },
+    {
+        'res': 'GLY',
+        'don': [['N', 'N', False, 0, 'CA']],
+        'acc': [['O', 'O', False, 0, 'C']]
     },
-    "MET": {
-        "donors": {
-            "name": ["N"],
-            "trail": [],
-            "trail": [["CA", "CB"]],
-            "element": ["N"],
-            "rotatable": [False],
-            "state": ["N"]
-        },
-        "acceptors": {
-            "name": ["O", "SD"],
-            "element": ["O", "S"],
-            "state": ["N", "N"]
-        },
+    {
+        'res': 'HIS',
+        'don': [['N', 'N', False, 0, 'CA'], ['ND1', 'N', False, 0, 'CG'], ['NE2', 'N', False, 0, 'CD2']],
+        'acc': [['O', 'O', False, 0, 'C'], ['ND1', 'N', False, 0, 'CG'], ['NE2', 'N', False, 0, 'CD2']]
     },
-    "THR": {
-        "donors": {
-            "name": ["N", "OG1"],
-            "trail": [],
-            "trail": [["CA", "CB"], ["CB", "CA"]],
-            "element": ["N", "O"],
-            "rotatable": [False, True],
-            "state": ["N", "N"]
-        },
-        "acceptors": {
-            "name": ["O", "OG1"],
-            "element": ["O", "O"],
-            "state": ["N", "N"]
-        },
+    {
+        'res': 'ILE',
+        'don': [['N', 'N', False, 0, 'CA']],
+        'acc': [['O', 'O', False, 0, 'C']]
     },
-    "ASN": {
-        "donors": {
-            "name": ["N", "ND2"],
-            "trail": [],
-            "trail": [["CA", "CB"], ["CG", "CB"]],
-            "element": ["N", "N"],
-            "rotatable": [False, False],
-            "state": ["N", "N"]
-        },
-        "acceptors": {
-            "name": ["O", "OD1"],
-            "element": ["O", "O"],
-            "state": ["N", "N"]
-        },
+    {
+        'res': 'LEU',
+        'don': [['N', 'N', False, 0, 'CA']],
+        'acc': [['O', 'O', False, 0, 'C']]
     },
-    "GLN": {
-        "donors": {
-            "name": ["N", "NE2"],
-            "trail": [],
-            "trail": [["CA", "CB"], ["CD", "CG"]],
-            "element": ["N", "N"],
-            "rotatable": [False, False],
-            "state": ["N", "N"]
-        },
-        "acceptors": {
-            "name": ["O", "OE1"],
-            "element": ["O", "O"],
-            "state": ["N", "N"]
-        },
+    {
+        'res': 'LYS',
+        'don': [['N', 'N', False, 0, 'CA'], ['NZ', 'N', True, 1, 'CE']],
+        'acc': [['O', 'O', False, 0, 'C']]
     },
-    "ALA": {
-        "donors": {
-            "name": ["N"],
-            "trail": [],
-            "trail": [["CA", "CB"]],
-            "element": ["N"],
-            "rotatable": [False],
-            "state": ["N"]
-        },
-        "acceptors": {
-            "name": ["O"],
-            "element": ["O"],
-            "state": ["N"]
-        },
+    {
+        'res': 'MET',
+        'don': [['N', 'N', False, 0, 'CA']],
+        'acc': [['O', 'O', False, 0, 'C'], ['SD', 'S', True, 0, 'CG']]
     },
-    "GLY": {
-        "donors": {
-            "name": ["N"],
-            "trail": [],
-            "trail": [["CA", "C"]],
-            "element": ["N"],
-            "rotatable": [False],
-            "state": ["N"]
-        },
-        "acceptors": {
-            "name": ["O"],
-            "element": ["O"],
-            "state": ["N"]
-        },
+    {
+        'res': 'PHE',
+        'don': [['N', 'N', False, 0, 'CA']],
+        'acc': [['O', 'O', False, 0, 'C']]
     },
-    "ILE": {
-        "donors": {
-            "name": ["N"],
-            "trail": [],
-            "trail": [["CA", "CB"]],
-            "element": ["N"],
-            "rotatable": [False],
-            "state": ["N"]
-        },
-        "acceptors": {
-            "name": ["O"],
-            "element": ["O"],
-            "state": ["N"]
-        },
+    {
+        'res': 'PRO',
+        'don': [],
+        'acc': [['O', 'O', False, 0, 'C']]
     },
-    "LEU": {
-        "donors": {
-            "name": ["N"],
-            "trail": [],
-            "trail": [["CA", "CB"]],
-            "element": ["N"],
-            "rotatable": [False],
-            "state": ["N"]
-        },
-        "acceptors": {
-            "name": ["O"],
-            "element": ["O"],
-            "state": ["N"]
-        },
+    {
+        'res': 'SER',
+        'don': [['N', 'N', False, 0, 'CA'], ['OG', 'O', True, 0, 'CB']],
+        'acc': [['O', 'O', False, 0, 'C'], ['OG', 'O', True, 0, 'CB']]
     },
-    "PRO": {
-        "donors": {
-            "name": [],
-            "trail": [],
-            "trail": [],
-            "element": [],
-            "rotatable": [],
-            "state": []
-        },
-        "acceptors": {
-            "name": ["O"],
-            "element": ["O"],
-            "state": ["N"]
-        },
+    {
+        'res': 'THR',
+        'don': [['N', 'N', False, 0, 'CA'], ['OG1', 'O', True, 0, 'CB']],
+        'acc': [['O', 'O', False, 0, 'C'], ['OG1', 'O', True, 0, 'CB']]
     },
-    "PHE": {
-        "donors": {
-            "name": ["N"],
-            "trail": [],
-            "trail": [["CA", "CB"]],
-            "element": ["N"],
-            "rotatable": [False],
-            "state": ["N"]
-        },
-        "acceptors": {
-            "name": ["O"],
-            "element": ["O"],
-            "state": ["N"]
-        },
+    {
+        'res': 'TRP',
+        'don': [['N', 'N', False, 0, 'CA'], ['NE1', 'N', False, 0, 'CD1']],
+        'acc': [['O', 'O', False, 0, 'C']]
     },
-    "TYR": {
-        "donors": {
-            "name": ["N", "OH"],
-            "trail": [],
-            "trail": [["CA", "CB"], ["CZ", "CE1"]],
-            "element": ["N", "O"],
-            "rotatable": [False, True],
-            "state": ["N", "N"]
-        },
-        "acceptors": {
-            "name": ["O", "OH"],
-            "element": ["O", "O"],
-            "state": ["N", "N"]
-        },
+    {
+        'res': 'TYR',
+        'don': [['N', 'N', False, 0, 'CA'], ['OH', 'O', False, 0, 'CZ']],
+        'acc': [['O', 'O', False, 0, 'C'], ['OH', 'O', False, 0, 'CZ']]
     },
-    "VAL": {
-        "donors": {
-            "name": ["N"],
-            "trail": [],
-            "trail": [["CA", "CB"]],
-            "element": ["N"],
-            "rotatable": [False],
-            "state": ["N"]
-        },
-        "acceptors": {
-            "name": ["O"],
-            "element": ["O"],
-            "state": ["N"]
-        },
+    {
+        'res': 'VAL',
+        'don': [['N', 'N', False, 0, 'CA']],
+        'acc': [['O', 'O', False, 0, 'C']]
     },
-    "G": {
-        "donors": {
-            "name": ["O2'", "N1", "N2", "O6"],
-            "trail": [],
-            "trail": [["C2'", "C1'"], ["C2", "N3"], ["C2", "N3"], ["C6", "C5"]],
-            "element": ["O", "N", "N", "O"],
-            "rotatable": [True, False, False, False],
-            "state": ["N", "N", "N", "T"]
-        },
-        "acceptors": {
-            "name": ["OP1", "OP2", "O5'", "O4'", "O3'", "O2'", "N3", "O6", "N7", "N1"],
-            "element": ["O", "O", "O", "O", "O", "O", "N", "O", "N", "N"],
-            "state": ["N", "N", "N", "N", "N", "N", "N", "N", "N", "T"]
-        }
-    },
-    "A": {
-        "donors": {
-            "name": ["O2'", "N6"],
-            "trail": [],
-            "trail": [["C2'", "C1'"], ["C6", "C5"]],
-            "element": ["O", "N"],
-            "rotatable": [True, False],
-            "state": ["N", "N"]
-        },
-        "acceptors": {
-            "name": ["OP1", "OP2", "O5'", "O4'", "O3'", "O2'", "N1", "N3", "N7"],
-            "element": ["O", "O", "O", "O", "O", "O", "N", "N", "N"],
-            "state": ["N", "N", "N", "N", "N", "N", "N", "N", "N"]
-        }
-    },
-    "C": {
-        "donors": {
-            "name": ["O2'", "N4"],
-            "trail": [],
-            "trail": [["C2'", "C1'"], ["C4", "C5"]],
-            "element": ["O", "N"],
-            "rotatable": [True, False],
-            "state": ["N", "N"]
-        },
-        "acceptors": {
-            "name": ["OP1", "OP2", "O5'", "O4'", "O3'", "O2'", "N3", "O2"],
-            "element": ["O", "O", "O", "O", "O", "O", "N", "O"],
-            "state": ["N", "N", "N", "N", "N", "N", "N", "N"]
-        }
-    },
-    "U": {
-        "donors": {
-            "name": ["O2'", "N3", "O2", "O4"],
-            "trail": [],
-            "trail": [["C2'", "C1'"], ["C2", "N1"], ["C2", "N1"], ["C4", "N3"]],
-            "element": ["O", "N", "O", "O"],
-            "rotatable": [True, False, False, False],
-            "state": ["N", "N", "T", "T"]
-        },
-        "acceptors": {
-            "name": ["OP1", "OP2", "O5'", "O4'", "O3'", "O2'", "O4", "O2", "N3"],
-            "element": ["O", "O", "O", "O", "O", "O", "O", "O", "N"],
-            "state": ["N", "N", "N", "N", "N", "N", "N", "N", "T"]
-        }
-    },
-    "DG": {
-        "donors": {
-            "name": ["N1", "N2", "O6"],
-            "trail": [],
-            "trail": [["C2", "N3"], ["C2", "N3"], ["C6", "C5"]],
-            "element": ["N", "N", "O"],
-            "rotatable": [False, False, False],
-            "state": ["N", "N", "T"]
-        },
-        "acceptors": {
-            "name": ["OP1", "OP2", "O5'", "O4'", "O3'", "N3", "O6", "N7", "N1"],
-            "element": ["O", "O", "O", "O", "O", "N", "O", "N", "N"],
-            "state": ["N", "N", "N", "N", "N", "N", "N", "N", "T"]
-        }
-    },
-    "DA": {
-        "donors": {
-            "name": ["N6"],
-            "trail": [],
-            "trail": [["C6", "C5"]],
-            "element": ["N"],
-            "rotatable": [False],
-            "state": ["N"]
-        },
-        "acceptors": {
-            "name": ["OP1", "OP2", "O5'", "O4'", "O3'", "N1", "N3", "N7"],
-            "element": ["O", "O", "O", "O", "O", "N", "N", "N"],
-            "state": ["N", "N", "N", "N", "N", "N", "N", "N"]
-        }
-    },
-    "DC": {
-        "donors": {
-            "name": ["N4"],
-            "trail": [],
-            "trail": [["C4", "C5"]],
-            "element": ["N"],
-            "rotatable": [False],
-            "state": ["N"]
-        },
-        "acceptors": {
-            "name": ["OP1", "OP2", "O5'", "O4'", "O3'", "N3", "O2"],
-            "element": ["O", "O", "O", "O", "O", "N", "O"],
-            "state": ["N", "N", "N", "N", "N", "N", "N"]
-        }
-    },
-    "DT": {
-        "donors": {
-            "name": ["N3", "O2", "O4"],
-            "trail": [],
-            "trail": [["C2", "N1"], ["C2", "N1"], ["C4", "N3"]],
-            "element": ["N", "O", "O"],
-            "rotatable": [False, False, False],
-            "state": ["N", "T", "T"]
-        },
-        "acceptors": {
-            "name": ["OP1", "OP2", "O5'", "O4'", "O3'", "O4", "O2", "N3"],
-            "element": ["O", "O", "O", "O", "O", "O", "O", "N"],
-            "state": ["N", "N", "N", "N", "N", "N", "N", "T"]
-        }
-    }
-}
+]
