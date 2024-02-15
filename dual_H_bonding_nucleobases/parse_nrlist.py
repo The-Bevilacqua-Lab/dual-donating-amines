dde9f173e3bf40fd2ca8e0ecdf875c95f8c686ff
drew161 <ajv6@psu.edu>
2024-02-15 16:41:50 -0500

    changed to a script that writes a csv file for every representative structure

--- dual_H_bonding_nucleobases/parse_nrlist.py
+++ dual_H_bonding_nucleobases/parse_nrlist.py
@@ -1,79 +1,64 @@
 """
-This module provides functions that work with the representative set file.
+This script iterates through the lines of a representative set file and collects the equivalence class names, PDB IDs,
+model info, and chain info for the representative structures. The info for each representative structure is then written
+to a csv file. The name of the representative set file must be provided as the first argument.
 """
 
 import sys
 import os
 import csv
 
-
-def identify():
-    """
-    This function searches for and returns the name of a representative set file in the current working directory.
-    If there is not exactly one representative set file, an error will be reported and the code will exit.
-    """
-    directory = os.getcwd()
-    nrlist_file = ""
-    file_list = os.listdir(directory)
-    num_nrlist = 0
-    for file in file_list:
-        if file[0:6] == "nrlist":
-            if file[-3:] == "csv":
-                num_nrlist += 1
-                nrlist_file = file
-    if num_nrlist == 0:
-        print("Error: There is no representative set file in the current working directory.")
+# check to make sure the representative set file exists
+nrlist_file = ""
+try:
+    if not os.path.isfile(sys.argv[1]):
+        print(f"Error: The file named {sys.argv[1]} was not found in the current working directory.")
         sys.exit(1)
-    if num_nrlist > 1:
-        print("Error: There is more than one representative set file in the current working directory.")
-        sys.exit(1)
-    return nrlist_file
+    else:
+        nrlist_file = sys.argv[1]
+except IndexError:
+    print(f"Error: The name of the representative set file must be provided as the first argument.")
+    sys.exit(1)
 
-
-def get_info(nrlist_file):
-    """
-    This function iterates through the lines of a representative set file and returns a list with the equivalence class
-    names and PDB IDs, model info, and chain info for the representative structures. The name of the representative set
-    file is a required argument.
-    """
-    nrlist_info = []
-    with open(nrlist_file, mode='r') as file:
-        for line in csv.reader(file):
-            PDB_ID_list = []
-            model_list = []
-            chain_list = []
-            for group in line[1].split('+'):
-                PDB_ID = []
-                model = []
-                chain = []
-                index = 0
-                for char in group:
-                    if char != '|' and index == 0:
-                        PDB_ID.append(char)
-                    elif char != '|' and index == 1:
-                        model.append(char)
-                    elif char != '|' and index == 2:
-                        chain.append(char)
-                    elif char == '|':
-                        index += 1
-                # check to ensure this part of the representative structure contains the expected amount of information
-                if index != 2:
-                    print(f"Error: The representative structure for equivalence class {line[0]} is missing "
-                          f"information.")
-                    sys.exit(1)
-                # check if the PDB ID contains the expected number of characters
-                if len("".join(PDB_ID)) != 4:
-                    print(f"Error: At least one of the PDB IDs for the representative structure of equivalence class "
-                          f"{line[0]} does not contain the expected number of characters.")
-                    sys.exit(1)
-                PDB_ID_list.append("".join(PDB_ID))
-                model_list.append("".join(model))
-                chain_list.append("".join(chain))
-            # check to make sure the PDB IDs match
-            for pdb in PDB_ID_list:
-                if pdb != PDB_ID_list[0]:
-                    print(f"Error: The PDB IDs for the representative structure of equivalence class {line[0]} do not "
-                          f"match.")
-                    sys.exit(1)
-            nrlist_info.append([line[0], PDB_ID_list, model_list, chain_list])
-    return nrlist_info
+# collect and write the information to csv files
+with open(nrlist_file, mode='r') as read_file:
+    for line in csv.reader(read_file):
+        PDB_ID_list = []
+        model_list = []
+        chain_list = []
+        for group in line[1].split('+'):
+            PDB_ID = []
+            model = []
+            chain = []
+            index = 0
+            for char in group:
+                if char != '|' and index == 0:
+                    PDB_ID.append(char)
+                elif char != '|' and index == 1:
+                    model.append(char)
+                elif char != '|' and index == 2:
+                    chain.append(char)
+                elif char == '|':
+                    index += 1
+            # check to ensure this part of the representative structure contains the expected amount of information
+            if index != 2:
+                print(f"Error: The representative structure for equivalence class {line[0]} is missing "
+                      f"information.")
+                sys.exit(1)
+            # check if the PDB ID contains the expected number of characters
+            if len("".join(PDB_ID)) != 4:
+                print(f"Error: At least one of the PDB IDs for the representative structure of equivalence class "
+                      f"{line[0]} does not contain the expected number of characters.")
+                sys.exit(1)
+            PDB_ID_list.append("".join(PDB_ID))
+            model_list.append("".join(model))
+            chain_list.append("".join(chain))
+        # check to make sure the PDB IDs match
+        for pdb in PDB_ID_list:
+            if pdb != PDB_ID_list[0]:
+                print(f"Error: The PDB IDs for the representative structure of equivalence class {line[0]} do not "
+                      f"match.")
+                sys.exit(1)
+        with open(f"{line[0]}_info.csv", "w") as write_file:
+            writer = csv.writer(write_file)
+            writer.writerow([line[0], PDB_ID_list, model_list, chain_list])
