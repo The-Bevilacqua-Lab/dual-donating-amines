93af7b8736b1b74655d9198444095fa1edf8dd54
drew161 <ajv6@psu.edu>
2024-03-26 12:38:09 -0400

    added eq_class column to csv output files and changed column names

--- workflow/scripts/process_data.py
+++ workflow/scripts/process_data.py
@@ -31,10 +31,10 @@ for residue in const.RESIDUE_LIBRARY:
                 don_acc_atoms.append((residue['res'], donor[0]))
 
 # extract the data from the hbond csv file and remove redundant lines
-hbond_col_names = ["don index", "don name", "don resn", "don resi", "don chain", "acc index", "acc name",
-                   "acc resn", "acc resi", "acc chain", "dist", "ang", "vertex", "hydrogen",
-                   "rotated side chain"]
-unique_col_comb = ["don index", "acc index", "hydrogen", "rotated side chain"]
+hbond_col_names = ["don_index", "don_name", "don_resn", "don_resi", "don_chain", "acc_index", "acc_name",
+                   "acc_resn", "acc_resi", "acc_chain", "dist", "ang", "vertex", "hydrogen",
+                   "rotated_side_chain"]
+unique_col_comb = ["don_index", "acc_index", "hydrogen", "rotated_side_chain"]
 hbond_data = (pd.read_csv(snakemake.input.hbond, names=hbond_col_names, comment="#", na_filter=False)
               .drop_duplicates(subset=unique_col_comb))
 
@@ -44,23 +44,23 @@ nuc_data = pd.read_csv(snakemake.input.nuc, names=nuc_col_names, comment="#", na
 
 # identify atom pairs that meet the H-bond criteria and include a donor of interest
 don_hbonds = (hbond_data[(hbond_data["dist"] <= H_DIST_MAX) & (hbond_data["ang"] >= 180.0 - H_ANG_TOL)]
-              .merge(pd.DataFrame(const.DONORS_OF_INTEREST, columns=["don resn", "don name"]), how='inner'))
+              .merge(pd.DataFrame(const.DONORS_OF_INTEREST, columns=["don_resn", "don_name"]), how='inner'))
 
 # identify atom pairs that meet the H-bond criteria and include a protonated donor of interest
 # values in the _merge column matching left_only indicate acceptors that cannot typically also donate an H-bond
 prot_don_hbonds_unfiltered = (hbond_data[(hbond_data["dist"] <= H_DIST_MAX) & (hbond_data["ang"] >= 180.0 - H_ANG_TOL)]
-                              .merge(pd.DataFrame(const.PROT_DONORS_OF_INTEREST, columns=["don resn", "don name"]),
+                              .merge(pd.DataFrame(const.PROT_DONORS_OF_INTEREST, columns=["don_resn", "don_name"]),
                                      how='inner')
-                              .merge(pd.DataFrame(don_acc_atoms, columns=["acc resn", "acc name"]), how='left',
+                              .merge(pd.DataFrame(don_acc_atoms, columns=["acc_resn", "acc_name"]), how='left',
                                      indicator=True))
 
 # Filter prot_don_hbonds_unfiltered such that only acceptors that cannot typically also donate an H-bond are included.
 # Also remove side chain acceptor atoms from ASN and GLN residues due to ambiguity in side chain conformation.
 prot_don_hbonds = (prot_don_hbonds_unfiltered[(prot_don_hbonds_unfiltered["_merge"] == "left_only") &
-                                              ~((prot_don_hbonds_unfiltered["acc resn"] == "ASN") &
-                                                (prot_don_hbonds_unfiltered["acc name"] == "OD1")) &
-                                              ~((prot_don_hbonds_unfiltered["acc resn"] == "GLN") &
-                                                (prot_don_hbonds_unfiltered["acc name"] == "OE1"))]
+                                              ~((prot_don_hbonds_unfiltered["acc_resn"] == "ASN") &
+                                                (prot_don_hbonds_unfiltered["acc_name"] == "OD1")) &
+                                              ~((prot_don_hbonds_unfiltered["acc_resn"] == "GLN") &
+                                                (prot_don_hbonds_unfiltered["acc_name"] == "OE1"))]
                    .drop(columns="_merge"))
 
 # identify atom pairs that meet the H-bond criteria and include an acceptor of interest
@@ -69,7 +69,7 @@ acc_hbonds = (hbond_data[((hbond_data["vertex"] == "hydrogen") & (hbond_data["di
                          ((hbond_data["vertex"] == "donor") & (hbond_data["dist"] <= DON_DIST_MAX) &
                                     (hbond_data["ang"] >= 109.5 - DON_ANG_TOL) &
                                     (hbond_data["ang"] <= 109.5 + DON_ANG_TOL))]
-              .merge(pd.DataFrame(const.ACCEPTORS_OF_INTEREST, columns=["acc resn", "acc name"]), how='inner'))
+              .merge(pd.DataFrame(const.ACCEPTORS_OF_INTEREST, columns=["acc_resn", "acc_name"]), how='inner'))
 
 # identify atom pairs that meet the H-bond criteria and include a deprotonated acceptor of interest
 # values in the _merge column matching left_only indicate donors that cannot typically also accept an H-bond
@@ -79,17 +79,17 @@ deprot_acc_hbonds_unfiltered = (hbond_data[((hbond_data["vertex"] == "hydrogen")
                                             (hbond_data["ang"] >= 109.5 - DON_ANG_TOL) &
                                             (hbond_data["ang"] <= 109.5 + DON_ANG_TOL))]
                                 .merge(pd.DataFrame(const.DEPROT_ACCEPTORS_OF_INTEREST,
-                                       columns=["acc resn", "acc name"]), how='inner')
-                                .merge(pd.DataFrame(don_acc_atoms, columns=["don resn", "don name"]), how='left',
+                                       columns=["acc_resn", "acc_name"]), how='inner')
+                                .merge(pd.DataFrame(don_acc_atoms, columns=["don_resn", "don_name"]), how='left',
                                        indicator=True))
 
 # Filter deprot_acc_hbonds_unfiltered such that only donors that cannot typically also accept an H-bond are included.
 # Also remove side chain donor atoms from ASN and GLN residues due to ambiguity in side chain conformation.
 deprot_acc_hbonds = (deprot_acc_hbonds_unfiltered[(deprot_acc_hbonds_unfiltered["_merge"] == "left_only") &
-                                                  ~((deprot_acc_hbonds_unfiltered["acc resn"] == "ASN") &
-                                                    (deprot_acc_hbonds_unfiltered["acc name"] == "ND2")) &
-                                                  ~((deprot_acc_hbonds_unfiltered["acc resn"] == "GLN") &
-                                                    (deprot_acc_hbonds_unfiltered["acc name"] == "NE2"))]
+                                                  ~((deprot_acc_hbonds_unfiltered["acc_resn"] == "ASN") &
+                                                    (deprot_acc_hbonds_unfiltered["acc_name"] == "ND2")) &
+                                                  ~((deprot_acc_hbonds_unfiltered["acc_resn"] == "GLN") &
+                                                    (deprot_acc_hbonds_unfiltered["acc_name"] == "NE2"))]
                      .drop(columns="_merge"))
 
 # Create a new list of H-bonding atom pairs involving the donors of interest with redundancy removed such that if
@@ -100,8 +100,8 @@ deprot_acc_hbonds = (deprot_acc_hbonds_unfiltered[(deprot_acc_hbonds_unfiltered[
 # simplicity, H-bonds involving the original side chain conformations of acceptor residues are prioritized over
 # situations where a dual H-bonding exocyclic amine could be inferred by discarding the atom pair involving the original
 # side chain conformation and keeping the atom pair involving the rotated side chain conformation.
-acc_grp = ["don index", "acc resn", "acc resi", "acc chain"]
-don_hbonds_nr = (don_hbonds[don_hbonds.groupby(acc_grp)["rotated side chain"]
+acc_grp = ["don_index", "acc_resn", "acc_resi", "acc_chain"]
+don_hbonds_nr = (don_hbonds[don_hbonds.groupby(acc_grp)["rotated_side_chain"]
                  .transform(lambda grp: grp.str.fullmatch("none") if any(grp.str.fullmatch("none")) else True)])
 
 # Create a new list of H-bonding atom pairs involving the acceptors of interest with redundancy removed such that if
@@ -110,12 +110,12 @@ don_hbonds_nr = (don_hbonds[don_hbonds.groupby(acc_grp)["rotated side chain"]
 # donation by TYR OH atoms is also removed. The TYR OH hydrogen can occupy two different conformations. If both
 # conformations are capable of forming an H-bond with the acceptor of interest, only the conformation with the shortest
 # hydrogen-acceptor distance is kept.
-don_grp = ["don resn", "don resi", "don chain", "acc index"]
-acc_hbonds_nr = (acc_hbonds[(((acc_hbonds["don resn"] != "TYR") &
-                              (acc_hbonds.groupby(don_grp)["rotated side chain"]
+don_grp = ["don_resn", "don_resi", "don_chain", "acc_index"]
+acc_hbonds_nr = (acc_hbonds[(((acc_hbonds["don_resn"] != "TYR") &
+                              (acc_hbonds.groupby(don_grp)["rotated_side_chain"]
                                .transform(lambda grp: grp.str.fullmatch("none")
                                           if any(grp.str.fullmatch("none")) else True))) |
-                             ((acc_hbonds["don resn"] == "TYR") &
+                             ((acc_hbonds["don_resn"] == "TYR") &
                               (acc_hbonds.groupby(don_grp)["dist"]
                                .transform(lambda grp: [mem == grp.min() for mem in grp]))))])
 
@@ -123,37 +123,37 @@ acc_hbonds_nr = (acc_hbonds[(((acc_hbonds["don resn"] != "TYR") &
 # the H-bonds from the latter nucleobases must involve both exocyclic amine hydrogens and at least two different
 # acceptors
 if don_hbonds_nr.size > 0:
-    don_indices = list(don_hbonds_nr.groupby("don index").groups.keys())
-    single_don_exo_amines = pd.Series(don_indices, index=don_indices, name="don index")[
-        (don_hbonds_nr.groupby("don index")["hydrogen"].nunique() == 1) |
-        (don_hbonds_nr.groupby("don index")["acc index"].nunique() == 1)]
-    dual_don_exo_amines = pd.Series(don_indices, index=don_indices, name="don index")[
-        (don_hbonds_nr.groupby("don index")["hydrogen"].nunique() == 2) &
-        (don_hbonds_nr.groupby("don index")["acc index"].nunique() >= 2)]
+    don_indices = list(don_hbonds_nr.groupby("don_index").groups.keys())
+    single_don_exo_amines = pd.Series(don_indices, index=don_indices, name="don_index")[
+        (don_hbonds_nr.groupby("don_index")["hydrogen"].nunique() == 1) |
+        (don_hbonds_nr.groupby("don_index")["acc_index"].nunique() == 1)]
+    dual_don_exo_amines = pd.Series(don_indices, index=don_indices, name="don_index")[
+        (don_hbonds_nr.groupby("don_index")["hydrogen"].nunique() == 2) &
+        (don_hbonds_nr.groupby("don_index")["acc_index"].nunique() >= 2)]
 else:
     single_don_exo_amines = pd.Series([])
     dual_don_exo_amines = pd.Series([])
 
 # write dataframes to csv files
-don_hbonds_nr.to_csv(snakemake.output.don_hbonds_nr, index=False)
-prot_don_hbonds.to_csv(snakemake.output.prot_don_hbonds, index=False)
-acc_hbonds_nr.to_csv(snakemake.output.acc_hbonds_nr, index=False)
-deprot_acc_hbonds.to_csv(snakemake.output.deprot_acc_hbonds, index=False)
-single_don_exo_amines.to_csv(snakemake.output.single_don_exo_amines, index=False)
-dual_don_exo_amines.to_csv(snakemake.output.dual_don_exo_amines, index=False)
+don_hbonds_nr.assign(eq_class=snakemake.wildcards.eq_class).to_csv(snakemake.output.don_hbonds_nr, index=False)
+prot_don_hbonds.assign(eq_class=snakemake.wildcards.eq_class).to_csv(snakemake.output.prot_don_hbonds, index=False)
+acc_hbonds_nr.assign(eq_class=snakemake.wildcards.eq_class).to_csv(snakemake.output.acc_hbonds_nr, index=False)
+deprot_acc_hbonds.assign(eq_class=snakemake.wildcards.eq_class).to_csv(snakemake.output.deprot_acc_hbonds, index=False)
+single_don_exo_amines.to_frame().assign(eq_class=snakemake.wildcards.eq_class).to_csv(snakemake.output.single_don_exo_amines, index=False)
+dual_don_exo_amines.to_frame().assign(eq_class=snakemake.wildcards.eq_class).to_csv(snakemake.output.dual_don_exo_amines, index=False)
 
 # Repeat the steps to prepare don_hbonds_nr but without the filtering using h-bonding criteria to create
 # don_hbonds_geom_nr. This data can be used to look at the distribution of h-bonding geometry measurements.
-acc_grp = ["don index", "acc resn", "acc resi", "acc chain"]
-don_hbonds_geom = hbond_data.merge(pd.DataFrame(const.DONORS_OF_INTEREST, columns=["don resn", "don name"]),
+acc_grp = ["don_index", "acc_resn", "acc_resi", "acc_chain"]
+don_hbonds_geom = hbond_data.merge(pd.DataFrame(const.DONORS_OF_INTEREST, columns=["don_resn", "don_name"]),
                                    how='inner')
-don_hbonds_geom_nr = (don_hbonds_geom[don_hbonds_geom.groupby(acc_grp)["rotated side chain"]
+don_hbonds_geom_nr = (don_hbonds_geom[don_hbonds_geom.groupby(acc_grp)["rotated_side_chain"]
                       .transform(lambda grp: grp.str.fullmatch("none") if any(grp.str.fullmatch("none")) else True)])
 
 # TODO consider creating csv file that only include one acceptor with closest D-H...A distance per donor
 # write H-bond geometry info related to don_hbonds_geom_nr to csv files
 if don_hbonds_geom_nr.size > 0:
-    don_acc_grp = ["don index", "acc index"]
+    don_acc_grp = ["don_index", "acc_index"]
     (don_hbonds_geom_nr[
          # only include hydrogens with smaller D-H...A distances
          (don_hbonds_geom_nr.groupby(don_acc_grp)["dist"]
@@ -164,10 +164,10 @@ if don_hbonds_geom_nr.size > 0:
          (don_hbonds_geom_nr.groupby(don_acc_grp)["dist"]
           .transform(lambda grp: [mem == grp.min() for mem in grp])) &
          # do not consider A(N6)-U(O4), C(N4)-G(O6), G(N2)-C(O2), and G(N2)-C(N3) atom pairs
-         (~don_hbonds_geom_nr[["don name", "acc resn", "acc name"]].eq(["N6", "U", "O4"]).all(axis='columns')) &
-         (~don_hbonds_geom_nr[["don name", "acc resn", "acc name"]].eq(["N4", "G", "O6"]).all(axis='columns')) &
-         (~don_hbonds_geom_nr[["don name", "acc resn", "acc name"]].eq(["N2", "C", "O2"]).all(axis='columns')) &
-         (~don_hbonds_geom_nr[["don name", "acc resn", "acc name"]].eq(["N2", "C", "N3"]).all(axis='columns'))]
+         (~don_hbonds_geom_nr[["don_name", "acc_resn", "acc_name"]].eq(["N6", "U", "O4"]).all(axis='columns')) &
+         (~don_hbonds_geom_nr[["don_name", "acc_resn", "acc_name"]].eq(["N4", "G", "O6"]).all(axis='columns')) &
+         (~don_hbonds_geom_nr[["don_name", "acc_resn", "acc_name"]].eq(["N2", "C", "O2"]).all(axis='columns')) &
+         (~don_hbonds_geom_nr[["don_name", "acc_resn", "acc_name"]].eq(["N2", "C", "N3"]).all(axis='columns'))]
      .to_csv(snakemake.output.don_hbonds_geom_filtered, index=False, columns=["dist", "ang"]))
 else:
     with open(snakemake.output.don_hbonds_geom, "w") as write_file:
@@ -195,7 +195,7 @@ else:
 # print(don_hbonds_nr[(don_hbonds_nr["ang"] < 133) & (don_hbonds_nr["ang"] > 132) & (don_hbonds_nr["dist"] > 2.8) & (don_hbonds_nr["dist"] < 2.85)][["dist", "ang"]])
 
 # # print dual H-bonding nucleobases that also accept H-bonds via their acc of interest
-# print(don_hbonds.merge(dual_don_exo_amines).merge(acc_hbonds_nr, left_on=["don resn", "don resi", "don chain"], right_on=["acc resn", "acc resi", "acc chain"]))
+# print(don_hbonds.merge(dual_don_exo_amines).merge(acc_hbonds_nr, left_on=["don_resn", "don_resi", "don_chain"], right_on=["acc_resn", "acc_resi", "acc_chain"]))
 
 # count = 0
 # for key in h_bond_to_acc_of_int.keys():
