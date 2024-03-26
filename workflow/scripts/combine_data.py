f951af88f91c81dbd4ae5d1b1eb9c8c96ab0d4a7
drew161 <ajv6@psu.edu>
2024-03-26 14:19:47 -0400

    combine_data.py now just writes to csv files rather than also creating variables that hold dataframes for potential analysis

--- workflow/scripts/combine_data.py
+++ workflow/scripts/combine_data.py
@@ -1,62 +1,55 @@
 """
-This module will eventually do something.
+This script will eventually do something.
 """
 
-import sys
 import os
 import pandas as pd
 
 # the working directory should house the analysis/ folder
 working_dir = os.getcwd()
 
-# set the directory pointing to the don_hbonds_geom/ folder
-don_hbonds_geom_dir = working_dir + "/analysis/don_hbonds_geom"
+# create a dictionary that will contain all the data categories and their associated files
+categories = {
+    "don_hbonds_nr": [],
+    "prot_don_hbonds": [],
+    "acc_hbonds_nr": [],
+    "deprot_acc_hbonds": [],
+    "single_don_exo_amines": [],
+    "dual_don_exo_amines": [],
+    "don_hbonds_geom": [],
+    "don_hbonds_geom_filtered": [],
+    "nuc_data": []
+}
 
-# prepare a list of files from the don_hbonds_geom/ folder to combine
-don_hbonds_geom_files = []
-for file in os.listdir(don_hbonds_geom_dir):
-    # select files with the correct suffix
-    if file[-20:] == "_don_hbonds_geom.csv":
-        # find out which files actually contain data
-        contains_data = False
-        with open(don_hbonds_geom_dir + "/" + file, "r") as read_file:
-            num_lines = 0
-            for lines in read_file:
-                if num_lines > 0:
-                    contains_data = True
-                    break
-                else:
-                    num_lines += 1
-        # only include files that contain data
-        if contains_data:
-            don_hbonds_geom_files.append(don_hbonds_geom_dir + "/" + file)
+# prepare a list of files from each category folder to combine
+for cat in categories.keys():
+    for file in os.listdir(working_dir + "/analysis/" + cat):
+        # select files with the correct suffix
+        if file[-(len(cat) + 5):] == "_" + cat + ".csv":
+            # find out which files actually contain data
+            contains_data = False
+            with open(working_dir + "/analysis/" + cat + "/" + file, "r") as read_file:
+                num_lines = 0
+                for lines in read_file:
+                    if num_lines > 0:
+                        contains_data = True
+                        break
+                    else:
+                        num_lines += 1
+            # only include files that contain data
+            if contains_data:
+                categories[cat].append(working_dir + "/analysis/" + cat + "/" + file)
 
-# prepare the new dataframe and write it to a csv file
-don_hbonds_geom = pd.concat([pd.read_csv(file) for file in don_hbonds_geom_files])
-don_hbonds_geom.to_csv("don_hbonds_geom_combined.csv", index=False)
+# create the combined/ folder to write csv files to
+os.mkdir(working_dir + "/combined")
 
-# set the directory pointing to the don_hbonds_geom_filtered/ folder
-don_hbonds_geom_filtered_dir = working_dir + "/analysis/don_hbonds_geom_filtered"
-
-# prepare a list of files from the don_hbonds_geom_filtered/ folder to combine
-don_hbonds_geom_filtered_files = []
-for file in os.listdir(don_hbonds_geom_filtered_dir):
-    # select files with the correct suffix
-    if file[-29:] == "_don_hbonds_geom_filtered.csv":
-        # find out which files actually contain data
-        contains_data = False
-        with open(don_hbonds_geom_filtered_dir + "/" + file, "r") as read_file:
-            num_lines = 0
-            for lines in read_file:
-                if num_lines > 0:
-                    contains_data = True
-                    break
-                else:
-                    num_lines += 1
-        # only include files that contain data
-        if contains_data:
-            don_hbonds_geom_filtered_files.append(don_hbonds_geom_filtered_dir + "/" + file)
-
-# prepare the new dataframe and write it to a csv file
-don_hbonds_geom_filtered = pd.concat([pd.read_csv(file) for file in don_hbonds_geom_filtered_files])
-don_hbonds_geom_filtered.to_csv("don_hbonds_geom_filtered_combined.csv", index=False)
+# create the combined dataframes and write to csv files
+pd.concat([pd.read_csv(file) for file in categories["don_hbonds_nr"]]).to_csv("combined/don_hbonds_nr_c.csv", index=False)
+pd.concat([pd.read_csv(file) for file in categories["prot_don_hbonds"]]).to_csv("combined/prot_don_hbonds_c.csv", index=False)
+pd.concat([pd.read_csv(file) for file in categories["acc_hbonds_nr"]]).to_csv("combined/acc_hbonds_nr_c.csv", index=False)
+pd.concat([pd.read_csv(file) for file in categories["deprot_acc_hbonds"]]).to_csv("combined/deprot_acc_hbonds_c.csv", index=False)
+pd.concat([pd.read_csv(file) for file in categories["single_don_exo_amines"]]).to_csv("combined/single_don_exo_amines_c.csv", index=False)
+pd.concat([pd.read_csv(file) for file in categories["dual_don_exo_amines"]]).to_csv("combined/dual_don_exo_amines_c.csv", index=False)
+pd.concat([pd.read_csv(file) for file in categories["don_hbonds_geom"]]).to_csv("combined/don_hbonds_geom_c.csv", index=False)
+pd.concat([pd.read_csv(file) for file in categories["don_hbonds_geom_filtered"]]).to_csv("combined/don_hbonds_geom_filtered_c.csv", index=False)
+pd.concat([pd.read_csv(file) for file in categories["nuc_data"]]).to_csv("combined/nuc_data_c.csv", index=False)
