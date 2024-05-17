09d3b4504a608bf1c5ab01635048806bc677423c
drew161 <ajv6@psu.edu>
2024-05-17 09:11:50 -0400

    added option to only include representative structures or to include all equivalence class members

--- workflow/scripts/parse_nrlist.py
+++ workflow/scripts/parse_nrlist.py
@@ -1,20 +1,18 @@
 """
-This script iterates through the lines of a representative set file, searching for a specific equivalence class, and
-collects the equivalence class name, PDB ID, model info, and chain info of the representative structures for the
-specified equivalence class. The info for each representative structure is written to a csv file. The input file name,
-output file name, and specified equivalence class are provided by the Snakefile. If commit_hash is set to true in the
-Snakemake configuration file, the commit hash of the repo will also be written within a commented line to the csv file
-if no uncommitted changes have been made to the repo.
+This script iterates through the lines of a representative set file and collects the equivalence class name, PDB ID,
+model info, and chain info of the representative structures for each equivalence class. The info for all equivalence
+classes is returned as a single dictionary. The name of the representative set file must be provided as the first
+argument. If True is provided as the second argument, information for all members (not just the representative
+structures) of the equivalence classes will be compiled.
 """
 
 import sys
 import csv
 
 
-def parse_nrlist(nrlist_file):
+def parse_nrlist(nrlist_file, all_members=False):
     eq_class = {}
     with open(nrlist_file, mode='r') as read_file:
-        count = 0
         for line in csv.reader(read_file):
             eq_class[line[0]] = []
             for class_member in line[2].split(','):
@@ -61,4 +59,6 @@ def parse_nrlist(nrlist_file):
                 eq_class[line[0]].append(f'{line[0]}_{pdb_id_list[0]}_{model_list[0]}')
                 for chain in chain_list:
                     eq_class[line[0]][-1] = eq_class[line[0]][-1] + "_" + chain
+                if not all_members:
+                    break
     return eq_class
