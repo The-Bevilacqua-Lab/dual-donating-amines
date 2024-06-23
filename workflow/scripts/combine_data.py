f2f48eae84ed54e9a192e839cf43d3ec533ffcfb
drew161 <ajv6@psu.edu>
2024-06-23 19:14:56 -0400

    continued changes with don_h_bond migration

--- workflow/scripts/combine_data.py
+++ workflow/scripts/combine_data.py
@@ -13,9 +13,6 @@ h_bond_combined = pd.DataFrame(columns=["don_index", "don_name", "don_resn", "do
                                         "h_acc_distance", "h_angle", "h_dihedral", "h_name", "eq_class_members"])
 nuc_combined = pd.DataFrame(columns=["don_resn", "don_resi", "don_chain", "eq_class_members"])
 b_factor_combined = pd.DataFrame(columns=["resn", "resi", "chain", "subset", "mean", "eq_class_members"])
-don_h_bonds_combined = pd.DataFrame(columns=["don_index", "don_name", "don_resn", "don_resi", "don_chain", "acc_index",
-                                             "acc_name", "acc_resn", "acc_resi", "acc_chain", "don_acc_distance",
-                                             "h_acc_distance", "h_angle", "h_dihedral", "h_name", "eq_class_members"])
 
 # Combine the dataframes from the different equivalence class members. Do not incorporate empty dataframes.
 for idx in range(len(snakemake.input.h_bond)):
@@ -24,7 +21,6 @@ for idx in range(len(snakemake.input.h_bond)):
         h_bond_df = pd.read_csv(snakemake.input.h_bond[idx], keep_default_na=False, na_values="NaN")
         nuc_df = pd.read_csv(snakemake.input.nuc[idx], keep_default_na=False, na_values="NaN")
         b_factor_df = pd.read_csv(snakemake.input.b_factor[idx], keep_default_na=False, na_values="NaN")
-        don_h_bonds_df = pd.read_csv(snakemake.input.don_h_bonds[idx], keep_default_na=False, na_values="NaN")
         if counts_combined.empty and not counts_df.empty:
             counts_combined = counts_df.copy()
         elif not counts_df.empty:
@@ -41,10 +37,6 @@ for idx in range(len(snakemake.input.h_bond)):
             b_factor_combined = b_factor_df.copy()
         elif not b_factor_df.empty:
             b_factor_combined = pd.concat([b_factor_combined, b_factor_df])
-        if don_h_bonds_combined.empty and not don_h_bonds_df.empty:
-            don_h_bonds_combined = don_h_bonds_df.copy()
-        elif not don_h_bonds_df.empty:
-            don_h_bonds_combined = pd.concat([don_h_bonds_combined, don_h_bonds_df])
     except FileNotFoundError:
         continue
 
@@ -53,4 +45,3 @@ counts_combined.to_csv(snakemake.output.counts, index=False, na_rep='NaN')
 h_bond_combined.to_csv(snakemake.output.h_bond, index=False, na_rep='NaN')
 nuc_combined.to_csv(snakemake.output.nuc, index=False, na_rep='NaN')
 b_factor_combined.to_csv(snakemake.output.b_factor, index=False, na_rep='NaN')
-don_h_bonds_combined.to_csv(snakemake.output.don_h_bonds, index=False, na_rep='NaN')
