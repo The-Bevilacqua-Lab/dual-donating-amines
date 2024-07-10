ac0c2b27dfcc4a0fa17b833b0d634a68cdf995d8
drew161 <ajv6@psu.edu>
2024-07-10 12:51:19 -0400

    improved density plot layout

--- workflow/scripts/create_plots.R
+++ workflow/scripts/create_plots.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(gridExtra)
 library(dplyr)
 library(tidyr)
 library(svglite)
@@ -12,7 +13,7 @@ df$geom <- factor(df$geom, levels = c(0, 1))
 df$h_bond <- factor(df$h_bond, levels = c(0, 1))
 df$type <- factor(df$type, levels = c(0, 1, 2))
 
-# Create plots depicting the heavy atom density around the donors of interest.
+#### HEAVY ATOM DENSITY PLOTS ####
 
 # Extract the rows from the main data frame relevant for these plots.
 density_df <- df %>% filter(DOI == 1) %>% 
@@ -30,38 +31,49 @@ volume_diff <- volume_2 - volume_1
 density_df$density_1 <- density_df$count_1/volume_1
 density_df$density_2 <- (density_df$count_2-density_df$count_1)/volume_diff
 
-# Combine the density columns by pivoting.
-density_df <- density_df %>% 
-  pivot_longer(c("density_1", "density_2"), 
-               names_to = "density_set", values_to = "density")
-density_df[density_df$density_set == "density_1", "density_set"] <- 
-  "Atoms \u2264 5 \uc5"
-density_df[density_df$density_set == "density_2", "density_set"] <- 
-  "5 \uc5 \u003c Atoms \u2264 10 \uc5"
-
 # Calculate samples sizes.
-density_summary <- density_df %>% summarise(n = n(), .by = c(type, density_set))
+density_summary <- density_df %>% summarise(n = n(), .by = c(type, don_resn))
 
 # Specify the variables for the plots.
 custom_colors <- RColorBrewer::brewer.pal(4, "Greens")
-x_labels <- c(
-  paste("0\nn = ", prettyNum(density_summary$n[1], big.mark = ","), sep = ""), 
-  paste("1\nn = ", prettyNum(density_summary$n[3], big.mark = ","), sep = ""),
-  paste("2\nn = ", prettyNum(density_summary$n[5], big.mark = ","), sep = ""))
-ratio <- (3/(max(density_df$density) - min(density_df$density)))*1.5 
-ylim <- c(min(density_df$density), max(density_df$density))
+ratio_1 <- (3/(max(density_df$density_1) - min(density_df$density_1)))*1.5
+ratio_2 <- (3/(max(density_df$density_2) - min(density_df$density_2)))*1.5
+ylim_1 <- c(-0.004, max(density_df$density_1))
+ylim_2 <- c(-0.004, max(density_df$density_2))
 
 # Create the plots.
-plot_density <- density_df %>% ggplot(aes(x=type, y=density, fill=type)) +
+plot_density_1 <- density_df %>% ggplot(aes(x=type, y=density_1, fill=type)) +
   geom_violin(show.legend = FALSE) +
   geom_boxplot(width = 0.2, alpha = 0, outlier.size = 1, show.legend = FALSE) +
-  coord_fixed(ratio = ratio, xlim = c(1, 3), ylim = ylim) +
-  xlab(element_blank()) +
+  geom_text(data = density_summary,
+            aes(x=type, y=-0.0025,
+                label = paste("n = ", prettyNum(n, big.mark = ","), sep = "")),
+            size = 10, size.unit = "pt", vjust = 1) +
+  coord_fixed(ratio = ratio_1, xlim = c(1, 3), ylim = ylim_1) +
+  ggtitle("Atoms \u2264 " + snakemake@config[["count_dist_1"]] + " \uc5") +
+  xlab("# of Amine H-Bonds") +
   ylab(expression(paste("Density (Atoms/\uc5\ub3)"))) +
-  scale_x_discrete(labels = x_labels) +
   scale_fill_manual(values = custom_colors[2:4]) +
   theme_bw(base_size = 10) +
-  facet_wrap( ~ density_set + don_resn, nrow = 2)
+  theme(plot.title = element_text(hjust = 0.5)) +
+  facet_wrap( ~ don_resn, nrow = 1)
+plot_density_2 <- density_df %>% ggplot(aes(x=type, y=density_2, fill=type)) +
+  geom_violin(show.legend = FALSE) +
+  geom_boxplot(width = 0.2, alpha = 0, outlier.size = 1, show.legend = FALSE) +
+  geom_text(data = density_summary,
+            aes(x=type, y=-0.0025,
+                label = paste("n = ", prettyNum(n, big.mark = ","), sep = "")),
+            size = 10, size.unit = "pt", vjust = 1) +
+  coord_fixed(ratio = ratio_2, xlim = c(1, 3), ylim = ylim_2) +
+  ggtitle(snakemake@config[["count_dist_1"]] + " \uc5 \u003c Atoms \u2264 " +
+            snakemake@config[["count_dist_2"]] + " \uc5") +
+  xlab("# of Amine H-Bonds") +
+  ylab(expression(paste("Density (Atoms/\uc5\ub3)"))) +
+  scale_fill_manual(values = custom_colors[2:4]) +
+  theme_bw(base_size = 10) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  facet_wrap( ~ don_resn, nrow = 1)
+plot_density <- grid.arrange(plot_density_1, plot_density_2, nrow = 2)
 
 # Write the plots.
 ggsave(snakemake@output[["density"]], plot = plot_density, width = 6.5,
