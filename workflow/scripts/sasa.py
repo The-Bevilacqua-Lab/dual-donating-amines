850e93d6de99a962d0586c4132ec8d85d0a9e757
Andrew Veenis <aveenis@outlook.com>
2025-07-22 10:37:15 -0400

    refactor sasa.py to make it more concise

--- workflow/scripts/sasa.py
+++ workflow/scripts/sasa.py
@@ -1,3 +1,9 @@
+"""
+The sasa function provided by this module takes a list of amine donors and calculates the solvent accessible surface
+area (SASA) of each amine. If the encompassing structure contains hydrogens, it is saved to a new CIF file without the
+hydrogens, the new structure is loaded, then the SASA calculations are performed.
+"""
+
 from Bio.PDB.MMCIFParser import MMCIFParser
 from Bio.PDB.SASA import ShrakeRupley
 from Bio.PDB.mmcifio import MMCIFIO, Select
@@ -17,12 +23,12 @@ def sasa(pdb_id, model, eq_class_mem_id, donor_list, original_mmcif_dir, h_omitt
             h_present = True
             break
 
-    # If there are no hydrogens, proceed with the SASA calculations.
-    if not h_present:
+    # Define a function to calculate the SASA values of the amines.
+    def calculate(donors, bio_pdb_structure, pymol_model):
         sr = ShrakeRupley()
-        sr.compute(structure)
+        sr.compute(bio_pdb_structure)
         sasa_list = []
-        for pymol_amine in donor_list:
+        for pymol_amine in donors:
             chain = pymol_amine[4]
             hetfield = " "
             # Residue numbers and insertion codes are reported together in pymol_amine[3]. If an insertion code is present,
@@ -31,27 +37,32 @@ def sasa(pdb_id, model, eq_class_mem_id, donor_list, original_mmcif_dir, h_omitt
                 resseq = int(pymol_amine[3])
                 icode = " "
             else:
-                resseq = int(pymol_amine[3][:-1])
-                icode = pymol_amine[3][-1]
                 # This module does not currently handle insertion codes that are more than one character in length.
                 if not pymol_amine[3][-2].isdigit():
                     return ("Error: There is an insertion code that is more than one character in length in "
                             f"{eq_class_mem_id}.")
+                resseq = int(pymol_amine[3][:-1])
+                icode = pymol_amine[3][-1]
             name = pymol_amine[1]
             # PyMOL represents a blank altloc as "" while Bio.PDB represents a blank altloc as " ".
             if pymol_amine[6] == "":
                 altloc = " "
             else:
                 altloc = pymol_amine[6]
-            bio_pdb_amine = structure[int(model)-1][chain][(hetfield, resseq, icode)][name]
-            # If the alternative location of bio_pdb_amine matches that of pymol_amine, get the coordinates of the atom. If
-            # not, select the correct alternative location and then get the coordinates.
+            bio_pdb_amine = bio_pdb_structure[int(pymol_model)-1][chain][(hetfield, resseq, icode)][name]
+            # If the alternative location of bio_pdb_amine matches that of pymol_amine, get the SASA of the atom. If
+            # not, select the correct alternative location and then get the SASA.
             if bio_pdb_amine.get_altloc() == altloc:
                 sasa_list.append(bio_pdb_amine.sasa)
             else:
                 bio_pdb_amine.disordered_select(altloc)
                 sasa_list.append(bio_pdb_amine.sasa)
         return sasa_list
+
+    # If there are no hydrogens, proceed with the SASA calculations.
+    if not h_present:
+        sasa_values = calculate(donor_list, structure, model)
+        return sasa_values
 
     # Otherwise, save the structure without hydrogens, load the new structure, and then perform the SASA calculations.
     else:
@@ -69,39 +80,8 @@ def sasa(pdb_id, model, eq_class_mem_id, donor_list, original_mmcif_dir, h_omitt
         io.save(f"{h_omitted_mmcif_dir}{eq_class_mem_id}.cif", AtomSelect())
 
         # Load the mmCIF file with hydrogens omitted.
-        structure = parser.get_structure(pdb_id, f"{h_omitted_mmcif_dir}{eq_class_mem_id}.cif")
+        new_structure = parser.get_structure(f"h_omitted_{pdb_id}", f"{h_omitted_mmcif_dir}{eq_class_mem_id}.cif")
 
         # Now, proceed with the SASA calculations.
-        sr = ShrakeRupley()
-        sr.compute(structure)
-        sasa_list = []
-        for pymol_amine in donor_list:
-            chain = pymol_amine[4]
-            hetfield = " "
-            # Residue numbers and insertion codes are reported together in pymol_amine[3]. If an insertion code is
-            # present, tease it apart from the residue number.
-            if pymol_amine[3][-1].isdigit():
-                resseq = int(pymol_amine[3])
-                icode = " "
-            else:
-                resseq = int(pymol_amine[3][:-1])
-                icode = pymol_amine[3][-1]
-                # This module does not currently handle insertion codes that are more than one character in length.
-                if not pymol_amine[3][-2].isdigit():
-                    return ("Error: There is an insertion code that is more than one character in length in "
-                            f"{eq_class_mem_id}.")
-            name = pymol_amine[1]
-            # PyMOL represents a blank altloc as "" while Bio.PDB represents a blank altloc as " ".
-            if pymol_amine[6] == "":
-                altloc = " "
-            else:
-                altloc = pymol_amine[6]
-            bio_pdb_amine = structure[int(model)-1][chain][(hetfield, resseq, icode)][name]
-            # If the alternative location of bio_pdb_amine matches that of pymol_amine, get the coordinates of the atom.
-            # If not, select the correct alternative location and then get the coordinates.
-            if bio_pdb_amine.get_altloc() == altloc:
-                sasa_list.append(bio_pdb_amine.sasa)
-            else:
-                bio_pdb_amine.disordered_select(altloc)
-                sasa_list.append(bio_pdb_amine.sasa)
-        return sasa_list
+        sasa_values = calculate(donor_list, new_structure, model)
+        return sasa_values
