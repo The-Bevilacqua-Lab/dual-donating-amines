916538ffe9a8fdcf851eb52f6b200e8d7a26b09c
Andrew Veenis <aveenis@outlook.com>
2025-08-12 16:26:18 -0400

    changed acc_pair_examples.R to identify co-occurring dual-donating amines in several scenarios

--- workflow/scripts/acc_pair_examples.R
+++ workflow/scripts/acc_pair_examples.R
@@ -36,15 +36,36 @@ sheared_ga_df <- merge(g_a_n7_h_bonding_df, combined_df,
   distinct(don_index.y, eq_class_member.y, .keep_all = TRUE)
 
 # Rename and remove select columns.
-sheared_ga_df <- sheared_ga_df[c('don_index.y', 'don_name.y', 'don_resi', 'don_chain', 'eta.y', 'theta.y', 
+sheared_ga_df <- sheared_ga_df[c('don_index.y', 'don_name.y', 'don_resn.y', 'don_resi', 'don_chain', 'eta.y', 'theta.y',
                                  'acc_pair_1_name', 'acc_pair_1_resi', 'acc_pair_1_chain', 'acc_pair_2_name', 
                                  'acc_pair_2_resi.y', 'acc_pair_2_chain.y', 'model', 'PDB', 'eq_class_member.y', 
                                  'type.y')] %>% 
-  rename(don_index=don_index.y, don_name=don_name.y, eta=eta.y, theta=theta.y, acc_pair_2_resi=acc_pair_2_resi.y, 
-         acc_pair_2_chain=acc_pair_2_chain.y, eq_class_member=eq_class_member.y, type=type.y)
+  rename(don_index=don_index.y, don_name=don_name.y, don_resn=don_resn.y, eta=eta.y, theta=theta.y,
+         acc_pair_2_resi=acc_pair_2_resi.y, acc_pair_2_chain=acc_pair_2_chain.y, eq_class_member=eq_class_member.y,
+         type=type.y)
 
 # Write the data to a csv.
 write.csv(sheared_ga_df, snakemake@output[["supplemental_data_S1"]], na = "NaN")
+
+# Now, find which guanines form a G(N2)-to-A(N7) H-bond and bear a dual-donating N2.
+g_a_n7_h_bonding_dual_df <- merge(guanines_to_consider_df, combined_df,
+                                  by.x = c('adenine_resi', 'adenine_chain', 'acc_pair_1_resi', 'acc_pair_1_chain',
+                                           'model', 'PDB'),
+                                  by.y = c('acc_resi', 'acc_chain', 'don_resi', 'don_chain', 'model', 'PDB')) %>%
+  filter(type == 2, h_bond == 1, don_name == "N2", acc_name == "N7") %>%
+  distinct(don_index, eq_class_member, .keep_all = TRUE)
+
+# Rename and remove select columns.
+g_a_n7_h_bonding_dual_df <- g_a_n7_h_bonding_dual_df[c('don_index', 'don_name', 'don_resn', 'acc_pair_1_resi',
+                                                       'acc_pair_1_chain', 'eta', 'theta', 'acc_pair_1_name.y',
+                                                       'acc_pair_1_resi.y', 'acc_pair_1_chain.y', 'acc_pair_2_name.y',
+                                                       'acc_pair_2_resi', 'acc_pair_2_chain', 'model', 'PDB',
+                                                       'eq_class_member', 'type')] %>%
+  rename(don_resi=acc_pair_1_resi, don_chain=acc_pair_1_chain, acc_pair_1_name=acc_pair_1_name.y,
+         acc_pair_1_resi=acc_pair_1_resi.y, acc_pair_1_chain=acc_pair_1_chain.y, acc_pair_2_name=acc_pair_2_name.y)
+
+# Write the data to a csv.
+write.csv(g_a_n7_h_bonding_dual_df, snakemake@output[["supplemental_data_S2"]], na = "NaN")
 
 #### 4B - WC/H A-MINOR MOTIF ####
 
@@ -83,7 +104,7 @@ a_minor_df <- merge(c_g_h_bonding_df, combined_df,
 a_minor_df <- a_minor_df %>% rename(acc_pair_1_resi=guanine_resi, acc_pair_1_chain=guanine_chain)
 
 # Write the data to a csv.
-write.csv(a_minor_df, snakemake@output[["supplemental_data_S2"]], na = "NaN")
+write.csv(a_minor_df, snakemake@output[["supplemental_data_S3"]], na = "NaN")
 
 # Which guanines form a G(N2)-to-A(N1) H-bond?
 g_n2_a_n1_h_bonding_df <- merge(a_minor_df, combined_df, 
@@ -103,7 +124,7 @@ g_n2_a_n1_h_bonding_filtered_df <- merge(a_minor_df, g_n2_a_n1_h_bonding_df,
   distinct(don_index, eq_class_member, .keep_all = TRUE)
 
 # Write the data to a csv.
-write.csv(g_n2_a_n1_h_bonding_filtered_df, snakemake@output[["supplemental_data_S3"]], na = "NaN")
+write.csv(g_n2_a_n1_h_bonding_filtered_df, snakemake@output[["supplemental_data_S4"]], na = "NaN")
 
 # Which guanines form a G(N2)-to-A(N7) H-bond?
 g_n2_a_n7_h_bonding_df <- merge(a_minor_df, combined_df, 
@@ -123,7 +144,7 @@ g_n2_a_n7_h_bonding_filtered_df <- merge(a_minor_df, g_n2_a_n7_h_bonding_df,
   distinct(don_index, eq_class_member, .keep_all = TRUE)
 
 # Write the data to a csv.
-write.csv(g_n2_a_n7_h_bonding_filtered_df, snakemake@output[["supplemental_data_S4"]], na = "NaN")
+write.csv(g_n2_a_n7_h_bonding_filtered_df, snakemake@output[["supplemental_data_S5"]], na = "NaN")
 
 # Which of the guanines also bear a dual donating G(N2)?
 dual_g_df <- merge(combined_df, c_g_h_bonding_df,
@@ -132,7 +153,7 @@ dual_g_df <- merge(combined_df, c_g_h_bonding_df,
   filter(don_resn == "G", type == 2) %>% distinct(don_index, eq_class_member, .keep_all = TRUE)
 
 # Write the data to a csv.
-write.csv(dual_g_df, snakemake@output[["supplemental_data_S5"]], na = "NaN")
+write.csv(dual_g_df, snakemake@output[["supplemental_data_S6"]], na = "NaN")
 
 #### 4C - A DUAL DONATING AMINE TO A HOOGSTEEN ####
 
@@ -170,7 +191,28 @@ tHH_aa_df <- tHH_aa_df[c('don_index.y', 'don_name.y', 'don_resi', 'don_chain', '
          acc_pair_2_chain=acc_pair_2_chain.y, eq_class_member=eq_class_member.y, type=type.y)
 
 # Write the data to a csv.
-write.csv(tHH_aa_df, snakemake@output[["supplemental_data_S6"]], na = "NaN")
+write.csv(tHH_aa_df, snakemake@output[["supplemental_data_S7"]], na = "NaN")
+
+# Now, find which adenines form an A(N6)-to-A(N7) H-bond and bear a dual-donating N6.
+a_n6_a_n7_h_bonding_dual_df <- merge(a_hoogsteen_df, combined_df,
+                                     by.x = c('dual_don_A_resi', 'dual_don_A_chain', 'acc_pair_1_resi',
+                                              'acc_pair_1_chain', 'model', 'PDB'),
+                                     by.y = c('acc_resi', 'acc_chain', 'don_resi', 'don_chain', 'model', 'PDB')) %>%
+  filter(type == 2, h_bond == 1, don_name == "N6", acc_name == "N7") %>%
+  distinct(don_index, eq_class_member, .keep_all = TRUE)
+
+# Rename and remove select columns.
+a_n6_a_n7_h_bonding_dual_df <- a_n6_a_n7_h_bonding_dual_df[c('don_index', 'don_name', 'don_resn', 'acc_pair_1_resi',
+                                                             'acc_pair_1_chain', 'eta', 'theta', 'acc_pair_1_name.y',
+                                                             'acc_pair_1_resi.y', 'acc_pair_1_chain.y',
+                                                             'acc_pair_2_name.y', 'acc_pair_2_resi',
+                                                             'acc_pair_2_chain', 'model', 'PDB', 'eq_class_member',
+                                                             'type')] %>%
+  rename(don_resi=acc_pair_1_resi, don_chain=acc_pair_1_chain, acc_pair_1_name=acc_pair_1_name.y,
+         acc_pair_1_resi=acc_pair_1_resi.y, acc_pair_1_chain=acc_pair_1_chain.y, acc_pair_2_name=acc_pair_2_name.y)
+
+# Write the data to a csv.
+write.csv(a_n6_a_n7_h_bonding_dual_df, snakemake@output[["supplemental_data_S8"]], na = "NaN")
 
 #### 4D - C AMINE DUAL DONATION AND GC BASE PAIR ####
 
@@ -207,7 +249,7 @@ base_pairing_cytosines_df <- merge(combined_df,
   distinct(don_index, eq_class_member, .keep_all = TRUE)
 
 # Write the data to a csv.
-write.csv(base_pairing_cytosines_df, snakemake@output[["supplemental_data_S7"]], na = "NaN")
+write.csv(base_pairing_cytosines_df, snakemake@output[["supplemental_data_S9"]], na = "NaN")
 
 #### 4E - G AMINE DUAL DONATION AND GC BASE PAIR ####
 
@@ -223,28 +265,48 @@ guanines_df[guanines_df['acc_pair_1_name'] != "O2", c('cytosine_resi', 'cytosine
   guanines_df[guanines_df['acc_pair_1_name'] != "O2", c('acc_pair_2_resi', 'acc_pair_2_chain')]
 
 # Only keep relevant columns.
-guanines_df <- guanines_df[c('don_resi', 'don_chain', 'cytosine_resi', 'cytosine_chain', 'model', 'PDB')]
+guanines_df <- guanines_df[c('don_resi', 'don_chain', 'cytosine_resi', 'cytosine_chain', 'model', 'PDB')] %>%
+  rename(guanine_resi=don_resi, guanine_chain=don_chain)
 
 # Which cytosines form a C(N4)-to-G(O6) H-bond?
 c_n4_g_o6_df <- merge(guanines_df, combined_df, 
-                      by.x = c('cytosine_resi', 'cytosine_chain', 'model', 'PDB'),
-                      by.y = c('don_resi', 'don_chain', 'model', 'PDB')) %>%
+                      by.x = c('cytosine_resi', 'cytosine_chain', 'guanine_resi', 'guanine_chain', 'model', 'PDB'),
+                      by.y = c('don_resi', 'don_chain', 'acc_resi', 'acc_chain', 'model', 'PDB')) %>%
   filter(h_bond == 1, acc_resn == "G", acc_name == "O6") %>% distinct(don_index, eq_class_member, .keep_all = TRUE)
-
-# Backtrack to identify the C's involved in GC base pairs.
-base_pairing_guanines_df <- merge(c_n4_g_o6_df, guanines_df, 
-                                  by = c('cytosine_resi', 'cytosine_chain', 'model', 'PDB'))
 
 # Make guanines the focus of the data frame (e.g., the type column for each row should be associated with the guanine, 
 # not the cytosine).
-base_pairing_guanines_df <- merge(combined_df, 
-                                  base_pairing_guanines_df[c('don_resi.y', 'don_chain.y', 'model', 'PDB')], 
-                                   by.x = c('don_resi', 'don_chain', 'model', 'PDB'),
-                                   by.y = c('don_resi.y', 'don_chain.y', 'model', 'PDB')) %>%
-  distinct(don_index, eq_class_member, .keep_all = TRUE)
+base_pairing_guanines_df <- merge(c_n4_g_o6_df[c('guanine_resi', 'guanine_chain', 'model', 'PDB')], combined_df,
+                                   by.x = c('guanine_resi', 'guanine_chain', 'model', 'PDB'),
+                                   by.y = c('don_resi', 'don_chain', 'model', 'PDB')) %>%
+  distinct(don_index, eq_class_member, .keep_all = TRUE) %>% rename(don_resi=guanine_resi, don_chain=guanine_chain)
+
+# Specify the columns to write to the output CSV file.
+output_columns <- c('don_index', 'don_name', 'don_resn', 'don_resi', 'don_chain', 'eta', 'theta', 'chi', 'type',
+                    'acc_pair_1_name', 'acc_pair_1_resi', 'acc_pair_1_chain',
+                    'acc_pair_2_name', 'acc_pair_2_resi', 'acc_pair_2_chain',
+                    'model', 'PDB', 'eq_class_member')
 
 # Write the data to a csv.
-write.csv(base_pairing_guanines_df, snakemake@output[["supplemental_data_S8"]], na = "NaN")
+write.csv(base_pairing_guanines_df[output_columns], snakemake@output[["supplemental_data_S10"]], na = "NaN")
+
+# Create new columns that specify the A(N3) acceptor.
+base_pairing_guanines_df[c('adenine_resi', 'adenine_chain')] <- NA
+base_pairing_guanines_df[base_pairing_guanines_df['acc_pair_1_name'] == "N3", c('adenine_resi', 'adenine_chain')] <-
+  base_pairing_guanines_df[base_pairing_guanines_df['acc_pair_1_name'] == "N3",
+                           c('acc_pair_1_resi', 'acc_pair_1_chain')]
+base_pairing_guanines_df[base_pairing_guanines_df['acc_pair_1_name'] != "N3", c('adenine_resi', 'adenine_chain')] <-
+  base_pairing_guanines_df[base_pairing_guanines_df['acc_pair_1_name'] != "N3",
+                           c('acc_pair_2_resi', 'acc_pair_2_chain')]
+
+# Identify the adenines that accept an H-bond from the dual-donating G(N2) and that also bear a dual-donating N6.
+a_n6_dual_df <- merge(combined_df, base_pairing_guanines_df[c('adenine_resi', 'adenine_chain', 'model', 'PDB')],
+                      by.x = c('don_resi', 'don_chain', 'model', 'PDB'),
+                      by.y = c('adenine_resi', 'adenine_chain', 'model', 'PDB')) %>%
+  filter(type == 2) %>% distinct(don_index, eq_class_member, .keep_all = TRUE)
+
+# Write the data to a csv.
+write.csv(a_n6_dual_df[output_columns], snakemake@output[["supplemental_data_S11"]], na = "NaN")
 
 #### 4F - G DUAL DONATING AMINE TO A(N7) AND NPO ####
 
@@ -254,7 +316,7 @@ g_a_n7_npo_same_df <- combined_df %>% filter(don_resn == "G", type == 2,
   distinct(don_index, eq_class_member, .keep_all = TRUE)
 
 # Write the data to a csv.
-write.csv(g_a_n7_npo_same_df, snakemake@output[["supplemental_data_S9"]], na = "NaN")
+write.csv(g_a_n7_npo_same_df, snakemake@output[["supplemental_data_S12"]], na = "NaN")
 
 #### S1A - C DUAL DONATING AMINE TO AA(O) AND G(O6) ####
 
@@ -263,7 +325,7 @@ c_g_aa_df <- combined_df %>% filter(don_resn == "C", type == 2, acc_pair_combine
   distinct(don_index, eq_class_member, .keep_all = TRUE)
 
 # Write the data to a csv.
-write.csv(c_g_aa_df, snakemake@output[["supplemental_data_S10"]], na = "NaN")
+write.csv(c_g_aa_df, snakemake@output[["supplemental_data_S13"]], na = "NaN")
 
 #### S1B - G DUAL DONATING AMINE TO AA(O) AND C(O2) ####
 
@@ -272,7 +334,7 @@ g_c_aa_df <- combined_df %>% filter(don_resn == "G", type == 2, acc_pair_combine
   distinct(don_index, eq_class_member, .keep_all = TRUE)
 
 # Write the data to a csv.
-write.csv(g_c_aa_df, snakemake@output[["supplemental_data_S11"]], na = "NaN")
+write.csv(g_c_aa_df, snakemake@output[["supplemental_data_S14"]], na = "NaN")
 
 #### PSEUDO-TORSION LOCATION 1, LOCATION 2, AND NEIGHBORS ####
 
@@ -323,14 +385,14 @@ write.csv(pair_1_df[,c("don_index", "don_name", "don_resn", "don_resi", "don_cha
                        "don_label", "acc_pair_combined_reformat", "same_resi", "type",
                        "PDB", "model", "eq_class_member",
                        "eta", "theta", "eta_translated", "theta_translated", "chi")],
-          snakemake@output[["supplemental_data_S12"]], quote = FALSE, na = "NaN", row.names = FALSE)
+          snakemake@output[["supplemental_data_S15"]], quote = FALSE, na = "NaN", row.names = FALSE)
 write.csv(pair_2_df[,c("don_index", "don_name", "don_resn", "don_resi", "don_chain",
                        "acc_pair_1_name", "acc_pair_1_resi", "acc_pair_1_chain",
                        "acc_pair_2_name", "acc_pair_2_resi", "acc_pair_2_chain",
                        "don_label", "acc_pair_combined_reformat", "same_resi", "type",
                        "PDB", "model", "eq_class_member",
                        "eta", "theta", "eta_translated", "theta_translated", "chi")],
-          snakemake@output[["supplemental_data_S13"]], quote = FALSE, na = "NaN", row.names = FALSE)
+          snakemake@output[["supplemental_data_S16"]], quote = FALSE, na = "NaN", row.names = FALSE)
 
 # Create a csv file with residues from Location 1 that are connected to the 5'-end of any residue from Location 2. This
 # approach may not work if an insertion code is used for this or the downstream residue. If this is the case, print an
@@ -338,7 +400,7 @@ write.csv(pair_2_df[,c("don_index", "don_name", "don_resn", "don_resi", "don_cha
 if (any(grepl("[a-zA-Z]", pair_1_df$don_resi)) | any(grepl("[a-zA-Z]", pair_2_df$don_resi))) {
   cat("Error: At least one residue within one of the pseudo-torsion locations contains an insertion code. The file
       named Supplemental_Data_S14.csv will be empty.")
-  write.csv(data.frame(), snakemake@output[["supplemental_data_S14"]], quote = FALSE, na = "NaN", row.names = FALSE)
+  write.csv(data.frame(), snakemake@output[["supplemental_data_S17"]], quote = FALSE, na = "NaN", row.names = FALSE)
 } else {
   neighbors_df <- merge(pair_1_df %>% mutate(downstream_resi = as.character(as.numeric(don_resi) + 1)), pair_2_df,
                         by.x = c("downstream_resi", "don_chain", "eq_class_member"),
@@ -349,5 +411,5 @@ if (any(grepl("[a-zA-Z]", pair_1_df$don_resi)) | any(grepl("[a-zA-Z]", pair_2_df
                                   "don_label", "acc_pair_combined_reformat", "same_resi", "type",
                                   "PDB", "model", "eq_class_member",
                                   "eta", "theta", "eta_translated", "theta_translated", "chi")]
-  write.csv(neighbors_df, snakemake@output[["supplemental_data_S14"]], quote = FALSE, na = "NaN", row.names = FALSE)
+  write.csv(neighbors_df, snakemake@output[["supplemental_data_S17"]], quote = FALSE, na = "NaN", row.names = FALSE)
 }
